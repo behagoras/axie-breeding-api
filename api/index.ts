d2d2e6af0a7c60b62a79860b863a2aa0cb19dbaa
David Behar Lombrozo <david.behar@improving.com>
2021-10-03 00:47:37 -0500

    eslint

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
-import { VercelApiHandler, VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelApiHandler, VercelRequest, VercelResponse } from '@vercel/node'
 // getStatus
 const getStatus: VercelApiHandler = (req: VercelRequest, res: VercelResponse) => {
   res.status(200).json({
-    status: "server is running",
-  });
-};
+    status: 'server is running'
+  })
+}
 
-export default getStatus;
+export default getStatus

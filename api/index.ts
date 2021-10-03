14e0d2d21d1523e44c679bcf6100dda11da0f513
David Behar Lombrozo <david.behar@improving.com>
2021-10-03 00:44:41 -0500

    remove unused info

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import { VercelApiHandler, VercelRequest, VercelResponse } from '@vercel/node';
+// getStatus
+const getStatus: VercelApiHandler = (req: VercelRequest, res: VercelResponse) => {
+  res.status(200).json({
+    status: "server is running",
+  });
+};
+
+export default getStatus;
